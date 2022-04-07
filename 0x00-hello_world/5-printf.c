0d755ffd533c80c69671af6c3a31d90cb496a46f
HenockKet <henock.3825@gmail.com>
2022-04-07 13:59:45 -0700

    added 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - doesn't return any value
+ * Return: 0 after the text is printed
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,"\n);
+return (0);
+}
