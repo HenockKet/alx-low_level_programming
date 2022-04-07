ede77280c449a3c98ea68b1e2fcc80b95c940684
HenockKet <henock.3825@gmail.com>
2022-04-07 13:44:09 -0700

    added 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include<stdio.h>
-int main(void)
-/*
- * main - doesn't return any value
-*/
+int main(void)/*main - doesn't return any value*/
+
 {
 char str1[] = "\"Programming is like building a multilingual puzzle";
 
