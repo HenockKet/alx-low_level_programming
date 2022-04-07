b352389893a666484948515e601f29c0a4cd7da3
HenockKet <henock.3825@gmail.com>
2022-04-07 15:25:35 -0700

    added 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
 int main(void)
 
 {
-printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+dprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 return (1);
 }
