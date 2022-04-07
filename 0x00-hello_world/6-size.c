1b09a72096e72e15fc7c2b2c958870bb884dcab9
HenockKet <henock.3825@gmail.com>
2022-04-07 14:24:02 -0700

    added 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,6 @@ char charType;
 
 printf("Size of a char: %zu bytes\n", sizeof(charType));
 printf("Size of an int: %zu bytes\n", sizeof(intType));
-printf("Size of a long: %zu bytes\n", sizeof(longType));
 printf("Size of a long long int: %zu byte\n", sizeof(doubleType));
 printf("Size of a float: %zu bytes\n", sizeof(floatType));
 return (0);
