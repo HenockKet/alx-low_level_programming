efaefb661303346f771e1f5a2d400d37ba40f83b
HenockKet <henock.3825@gmail.com>
2022-04-07 15:03:52 -0700

    added 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - has no value
+ * Return: 1 after the text is printed to stderr
+ */
+int main(void)
+
+{
+fprintf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
+}
