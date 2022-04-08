a53455ff89dea1917e95b8ef0d7fbf0054ee7b20
HenockKet <henock.3825@gmail.com>
2022-04-08 15:25:24 -0700

    added multiple file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ * main - has no value
+ * Return: 0 after the numbers are printed
+*/
+int main (void)
+{
+int ch;
+for ( ch = '1' ; ch <= '9' ; ch++)
+putchar(ch);
+putchat('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - has no value
+ * Return: 0 after the print
+*/
+int main(void)
+{
+int ch;
+for (ch = '0' ; ch <= '9'; ch++)
+        putchar(ch);
+for (ch = 'a' ; ch <= 'f' ; ch++)
+        putchar(ch);
+putchar('\n');
+}
