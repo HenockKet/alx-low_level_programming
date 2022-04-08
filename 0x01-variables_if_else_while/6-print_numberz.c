2d1f8dfc510c0ff1dffb85bc2779a5c4464fb239
HenockKet <henock.3825@gmail.com>
2022-04-08 15:28:14 -0700

    added multiple file

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,10 +3,10 @@
  * main - has no value
  * Return: 0 after the numbers are printed
 */
-int main (void)
+int main(void)
 {
 int ch;
-for ( ch = '1' ; ch <= '9' ; ch++)
+for (ch = '0' ; ch <= '9' ; ch++)
 putchar(ch);
 putchar('\n');
 return (0);
