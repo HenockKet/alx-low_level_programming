69bc4ca33914f1d93e71a56c4ebc1bfba8d3efe5
HenockKet <henock.3825@gmail.com>
2022-04-08 14:40:28 -0700

    added multiple files

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
-/*
+/**
  * main - has no value
  * Return: 0 after the function ends
 */
 int main(void)
 {
-char ch;
+char c;
 
-char c = 'a';
-for(ch = 'a'; ch <= 'z'; ch++)
-if (ch != 'e' && ch != 'q')
-putchar(ch);
+c = 'a';
+for(c = 'a'; c <= 'z'; c++)
+if (c != 'e' && c != 'q')
+putchar(c);
 putchar('\n');
 return (0);
 }
