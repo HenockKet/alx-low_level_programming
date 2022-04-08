6a263f4fc09c7f60196ad1b50817e4959320f2fa
HenockKet <henock.3825@gmail.com>
2022-04-08 16:41:35 -0700

    added multiple files

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+/**
+ * main - has no value
+ * Return: 0
+*/
+int main(void)
+{
+int n1, n2;
+int c1 = 0;
+int c2 = 0;
+
+n1 = 48;
+while (n1 < 58)
+{
+n2 = n1 + 1;
+while (n2 < 58)
+{
+putchar(c1);
+putchar(c2);
+putchar(n1);
+putchar(n2);
+n2++;
+c1 = ',';
+c2 = ' ';
+}
+n1++;
+}
+
+putchar('\n');
+return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - has no value
+ * Return: 0 after the numbers are printed
+*/
+int main(void)
+{
+int ch;
+
+ch = 0;
+for (ch = '0'; ch <= '9'; ch++){
+putchar(ch);
+putchar(',');
+putchar(' ');
+}
+putchar('\n');
+return (0);
+}
