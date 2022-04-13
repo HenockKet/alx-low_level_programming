9e51b55b6895086e723ce524ab06df75f11c4417
HenockKet <henock.3825@gmail.com>
2022-04-13 07:46:06 -0700

    added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - print alphabet
+ * Return: 0 at end of excution
+*/
+int main(void)
+{
+char ch;
+ch = 'a';
+for(ch = 'a'; ch <= 'z'; ch++)
+_putchar(ch); 
+_putchar('\n');
+return (0);
+}
