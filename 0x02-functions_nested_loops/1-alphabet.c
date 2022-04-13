d900a892bcf4607dda7b7bd3139a8ce185deea8a
HenockKet <henock.3825@gmail.com>
2022-04-13 07:53:54 -0700

    added 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,12 +3,11 @@
  * main - print alphabet
  * Return: 0 at end of excution
 */
-int main(void)
+void print_alphabet(void)
 {
 char ch;
 ch = 'a';
 for(ch = 'a'; ch <= 'z'; ch++)
 _putchar(ch); 
 _putchar('\n');
-return (0);
 }
