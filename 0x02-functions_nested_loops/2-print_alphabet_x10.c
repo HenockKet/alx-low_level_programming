f66b72a242026cf35ac6612be0c5e109df2e4917
HenockKet <henock.3825@gmail.com>
2022-04-13 08:35:03 -0700

    added 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ * Return:  has no return value
+*/
+void print_alphabet_x10(void)
+{
+int i = 0;
+char ch = 'a';
+for(i = 0 ; i <= 9; i++)
+{
+for(ch = 'a' ; ch <= 'z'; ch++)
+_putchar(ch);
+_putchar('\n');
+}
+}
