22c7970de208d70047207442fac3313a7645f12d
HenockKet <henock.3825@gmail.com>
2022-04-14 18:07:46 -0700

    added files

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,10 @@
+#include <ctype.h>
+
+int _isupper(int i)
+{
+char c;
+
+if ( c <= 'A' && c >= 'Z')
+	return (1);
+
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * muk - multiply two numbers
+ * Return: the product of the numbers
+ */
+int mul(int a, int b)
+{
+int p;
+p = a*b;
+}
