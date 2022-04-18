9f26967e5da19cb86015f48c04f2fd3b29e9bb42
HenockKet <henock.3825@gmail.com>
2022-04-19 01:50:33 +0300

    added multiple files

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,6 +1,7 @@
 /**
  * swap_int - swaps values of two integers
- *
+ * @a: firet integer
+ * @b: second integer
  * Return: has no return
  */
 void swap_int(int *a, int *b)
@@ -8,6 +9,6 @@ void swap_int(int *a, int *b)
 int ab;
 
 ab = *a;
-a* = *b;
+*a = *b;
 *b = ab;
 }
