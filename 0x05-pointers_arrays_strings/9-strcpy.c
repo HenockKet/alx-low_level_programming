fe8e355bb5fbbdc7198e5be32119babc2292772b
HenockKet <henock.3825@gmail.com>
2022-04-20 01:47:03 +0300

    added file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,12 @@
+#include "string.h"
+/**
+ * *_strcpy - copy string form src to dest
+ * @src: given sting to be copied
+ * @dest: copied string
+ * Return: dest/copied string
+ */
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+return (dest);
+}
