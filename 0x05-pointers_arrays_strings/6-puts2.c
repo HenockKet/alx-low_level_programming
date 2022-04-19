c779e685832276fa0badd93ba4599b47669eee8d
HenockKet <henock.3825@gmail.com>
2022-04-19 20:52:51 +0300

    added multiple files

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character in string
+ * @str: given string
+ * Return: void
+ */
+void puts2(char *str)
+{
+int i;
+
+for (i = 0; i <= strlen(str); i+=2)
+_putchar(str[i]);
+_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include <string.h>
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: given string
+ * Return: void
+ */
+void puts_half(char *str)
+{
+int i;
+int l;
+int m;
+
+l = strlen(str);
+m = strlen(str) / 2;
+
+for(i = m; i <= l; i++)
+_putchar(str[i]);
+_putchar('\n');
+}
