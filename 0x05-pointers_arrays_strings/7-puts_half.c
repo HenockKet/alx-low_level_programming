75e9dd9d8d61040221302c64d338a97aaa9fef2e
HenockKet <henock.3825@gmail.com>
2022-04-19 22:29:45 +0300

    added multiple files

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,12 +14,12 @@ int m;
 l = strlen(str);
 if ( l % 2 == 0){
 m = strlen(str) / 2;
-for (i = m; i <= l; i++)
+for (i = m; i <= l - 1; i++)
 _putchar(str[i]);
 _putchar('\n');}
 else{
 m = (strlen(str) - 1) / 2;
-for (i = m; i <= l; i++)
+for (i = m; i <= l - 1; i++)
 _putchar(str[i]);
 _putchar('\n');
 }
