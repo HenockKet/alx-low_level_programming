b76b62578e519523756a3f09f7a4586b9c17d7d8
HenockKet <henock.3825@gmail.com>
2022-04-20 02:47:37 +0300

    added file

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+/**
+ * _atoi - returns int from string
+ * @s: given string
+ * Return: int from given string
+ */
+int _atoi(char *s)
+{
+int i;
+
+i = atoi(s);
+return (i);
+}
