744cab3c001e5d35d19357adf265805605f03fb2
HenockKet <henock.3825@gmail.com>
2022-04-22 00:41:39 +0300

    added multiple files

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+char *string_toupper(char *str)
+/**
+ * *string_toupper - convert string to uppercase
+ * @str - given string
+ * Return - converted str
+ */
+{
+int i;
+
+for (i = 0; str[i] != '\0' ; i++)
+if (str[i] > 96 && str[i] < 123)
+{
+(str[i] -= 32);
+}
+return (str);
+}
