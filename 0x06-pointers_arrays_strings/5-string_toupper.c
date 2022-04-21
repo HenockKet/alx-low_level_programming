e62ac3ccea182e9084153e55d49b7139d7de5418
HenockKet <henock.3825@gmail.com>
2022-04-22 00:44:37 +0300

    added multiple files

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,7 +8,8 @@ char *string_toupper(char *str)
 int i;
 
 for (i = 0; str[i] != '\0' ; i++)
-if (str[i] > 96 && str[i] < 123){
+if (str[i] > 96 && str[i] < 123)
+{
 (str[i] -= 32);
 }
 return (str);
